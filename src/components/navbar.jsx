d7b0d71b34e802a83d329d46900327e54c949d98
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-07 13:32:17 +0200

    feat(Navbar): add two buttons to the navbar and include them in the Link component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,18 @@
+import { ShoppingCart } from "phosphor-react";
+import {Link} from "react-router-dom";
+
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className="navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32}/>
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
