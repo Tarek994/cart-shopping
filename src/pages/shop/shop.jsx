d66baba76b4616e63f55efb3a53ff2d288b3862d
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-07 23:28:29 +0200

    feat(Shop): create a new component for the Shop section

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,17 @@
+
+import "./shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
+
+      <div className="products">
+        
+      </div>
+    </div>
+  );
+};
+export default Shop;
