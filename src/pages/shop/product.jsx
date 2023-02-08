5b080e4e6db359044f1e208474773e10a428e538
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-08 23:58:12 +0200

    feat(product.jsx): add the logec of the 'addToCart' button

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,23 +1,26 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
 
-    const { id, productName, price, productImage } = props.data;
-
+  const cartItemCount = cartItems[id];
 
   return (
     <div className="product">
-        <img alt="" src={productImage}/>
-        <div className="description"> 
-            <p>
-               <b>{productName}</b>
-            </p>
-            <p>${price}</p>
-        </div>
-        <button className="addToCartBttn">
-        Add To Cart
+      <img src={productImage} alt=""/>
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default Product;
