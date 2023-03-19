db0f81af9f4462a81f0abcde9e7024e449ae81d5
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-20 00:28:31 +0200

    feat(navbar.jsx): wrap the component with a new div

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="links">
-        <Link to="/">Shop</Link>
-        <Link to="/cart">
-          <ShoppingCart size={32} />
-        </Link>
+      <div className="main-nav">
+        <div className="logo">T.PRESS</div>
+        <div className="links">
+          <Link to="/">Shop</Link>
+          <Link to="/cart">
+            <ShoppingCart size={32} />
+          </Link>
+        </div>
       </div>
     </div>
   );
